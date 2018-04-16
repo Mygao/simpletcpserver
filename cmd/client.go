6c40d65b92f0c36a17db5234956708c7cf87af5d
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-04-16 22:43:47 +0900

    Add simple client for demo

---
+++ cmd/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+  "net"
+  "bufio"
+)
+
+var Addr = flag.String("addr", ":8080", "Server listening address")
+
+func main() {
+  conn, err := net.Dial("tcp", *Addr)
+  if err != nil {
+    log.Fatal(err)
+  }
+  for {
+    ret, err := bufio.NewReader(conn).ReadString('\n')
+    if err != nil {
+      log.Fatal(err)
+    }
+    log.Print(ret)
+  }
+}
