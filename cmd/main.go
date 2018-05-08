473dbc733ef71a8547c628d2f6ae0ccbd003e739
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-05-08 14:37:26 +0900

    Instead of continuous ping, send message by user input

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	server "github.com/aiqu/simpletcpserver"
 	"log"
-	"time"
+	"os"
+	"strings"
 )
 
 var Addr = flag.String("addr", ":8080", "Server listening address")
 
+func printHelpMessage() {
+	log.Print("Press key to send signal\n")
+	log.Print("p - send ping to all client\n")
+	log.Print("r - send start signal to all client\n")
+	log.Print("s - send stop signal to all client\n")
+}
+
 func main() {
-  flag.Parse()
+	flag.Parse()
 	s, err := server.New(*Addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		time.Sleep(time.Second)
-		s.PingAll()
+		printHelpMessage()
+		text, _ := reader.ReadString('\n')
+		switch {
+		case strings.Compare(text, "p\n") == 0:
+			s.SendAll("ping")
+		case strings.Compare(text, "r\n") == 0:
+			s.SendAll("start")
+		case strings.Compare(text, "s\n") == 0:
+			s.SendAll("stop")
+		}
 	}
 }
